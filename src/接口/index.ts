8c019ae61b14cc5db439b340811efebf4ab3ed85
Ronin <2254314552@qq.com>
2022-04-19 09:19:23 +0800

    了ing

--- src/接口/index.ts
+++ src/接口/index.ts
@@ -88,3 +88,32 @@ class Clock implements ClassInterface {
 let clock = new Clock(1, 2);
 console.log(clock.currentTime);
 console.log(clock.setTime(new Date()));
+
+// 定义一个接口，接口是构造函数类型
+interface ClockContructor {
+  new (hour: number, minute: number): ClockInterface;
+}
+interface ClockInterface {
+  tick(): void;
+}
+
+// 通过 ctor 进行检验类的 constructor 的类型是否符合标准
+function createClock(ctor: ClockContructor, hour: number, minute: number): ClockInterface {
+  return new ctor(hour, minute);
+}
+
+class DigitalClock implements ClockInterface {
+  constructor(h: number, m: number) {}
+  tick() {
+    console.log('beep beep');
+  }
+}
+class AnalogClock implements ClockInterface {
+  constructor(h: number, m: number) {}
+  tick() {
+    console.log('tick tock');
+  }
+}
+
+createClock(DigitalClock, 12, 12);
+createClock(AnalogClock, 12, 12);
