2d5d4d5fe7a401d644d108e2eab5e80348171e59
Ronin <2254314552@qq.com>
2022-04-21 11:28:24 +0800

    提交

--- src/装饰器/index.ts
+++ src/装饰器/index.ts
@@ -1,8 +1,15 @@
 function sealed(value: string) {
+  console.log('one');
+
+  return function (target: Function) {};
+}
+function desc(bole: boolean) {
+  console.log('two');
   return function (target: Function) {};
 }
 
 @sealed('sealed')
+@desc(false)
 class Cloth {
   greeting: string;
   constructor(message: string) {
